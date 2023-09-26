67b61ccdf5c95a575face60208382e5d01a75211
daniel endaylalu <endaylaludaniel@gmail.com>
2023-09-26 11:22:19 +0300

    finished unicafe react app

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Statistics from "./Statistics";
+import Button from "./Button";
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -6,6 +8,7 @@ const App = () => {
   const [bad, setBad] = useState(0);
   const total = good + neutral + bad;
   const avrage = (good * 1 + neutral * 0 + bad * -1) / total;
+  const positive = (good * 100) / total;
 
   const handleClick = (setItem, item) => {
     setItem(item + 1);
@@ -14,16 +17,22 @@ const App = () => {
   return (
     <div>
       <h1>give a feedback</h1>
-      <button onClick={() => handleClick(setGood, good)}>good</button>
-      <button onClick={() => handleClick(setNeutral, neutral)}>nutral</button>
-      <button onClick={() => handleClick(setBad, bad)}>bad</button>
+      <Button handleClick={() => handleClick(setGood, good)} name="good" />
+      <Button
+        handleClick={() => handleClick(setNeutral, neutral)}
+        name="neutral"
+      />
+      <Button handleClick={() => handleClick(setBad, bad)} name="bad" />
+
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>avrage {avrage}</p>
-      <p>positive {(good * 100) / total}</p>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        avrage={avrage}
+        positive={positive}
+      />
     </div>
   );
 };
